49d81799a8dd57d12c7577c3033f41c28440bc42
LucasKurt <lucascorte@outlook.com>
2023-10-03 14:59:25 -0300

    populate list of pokemons

--- src/app/services/poke-api.service.ts
+++ src/app/services/poke-api.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, map, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,10 +12,16 @@ export class PokeApiService {
 
   get findAll(): Observable<any> {
     return this.http.get<any>(this.url).pipe(
-      tap((res) => res),
+      // tap((res) => res),
       tap((res) => {
-        console.log(res);
+        res.results.map((pokemon: any) => {
+          this.getPokemonByUrl(pokemon.url).subscribe((res) => pokemon.status = res);
+        });
       })
     );
+  }
+
+  public getPokemonByUrl(url: string): Observable<any> {
+    return this.http.get<any>(url).pipe(map((res) => res));
   }
 }
